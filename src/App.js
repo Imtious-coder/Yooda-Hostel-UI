96610c617e828547645fc39d085a6d7c1fa64f31
Imtious-coder <iarafat386@gmail.com>
2022-02-10 15:32:38 +0600

    Edit,Delete Student part complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Edit_Student from './Components/Edit_Student/Edit_Student';
 import Foods from './Components/Foods/Foods';
 import Food_Form from './Components/Food_Form/Food_Form';
 import Home from './Components/Home/Home';
+import Serve_Form from './Components/Serve_Form/Serve_Form';
 import Students from './Components/Students/Students';
 import Student_Form from './Components/Student_Form/Student_Form';
 
@@ -15,9 +17,9 @@ function App() {
         <Route path="/show-foods" element={<Foods />} />
         <Route path="/add-students" element={<Student_Form />} />
         <Route path="/show-students" element={<Students />} />
-        {/*<Route path="/show-students/:id" element={<EditStudent />} />
-        <Route path="/distribution-food" element={<DistributeFood />} />
-        <Route path="/serve-students/:id/:shift" element={<ServeForm />} /> */}
+        <Route path="/show-students/:id" element={<Edit_Student />} />
+        {/* <Route path="/distribution-food" element={<DistributeFood />} /> */}
+        <Route path="/serve-students/:id/:shift" element={<Serve_Form />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Components/Serve_Form/Serve_Form.js
@@ -0,0 +1,82 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import { Alert, Button, Form } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
+import { useLocation, useParams } from 'react-router-dom';
+import Admin_Page from '../Admin_Page/Admin_Page';
+
+const Serve_Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const { id, shift } = useParams();
+  const location = useLocation();
+
+  // loading and success state
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(null);
+
+  const onSubmit = (data) => {
+    data = {
+      ...data,
+      studentId: id,
+      date: location.search.split('=')[1],
+      shift: shift,
+      status: true,
+    };
+    setIsLoading(true);
+    axios
+      .post(`http://localhost:5001/distribution-food`, data)
+      .then((res) => {
+        if (res.data.acknowledged === true) {
+          setIsLoading(false);
+          setIsSuccess(true);
+          reset({});
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsSuccess(false);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  return (
+    <Admin_Page pageTitle={'Serve Food'}>
+      {!!isSuccess && <Alert variant="success">Student Food Served!</Alert>}
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        {/* Food Item */}
+        <Form.Group className="mb-3" controlId="foodItemList">
+          <Form.Label>Enter Food Items</Form.Label>
+          <Form.Control
+            type="textbox"
+            {...register('foodItemList', { required: true })}
+            placeholder="Enter food items"
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.foodItemList && <span>This field is required</span>}
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        {isLoading ? (
+          <Button variant="outline-primary" type="submit" disabled>
+            Serve Student
+          </Button>
+        ) : (
+          <Button variant="outline-primary" type="submit">
+            Serve Student
+          </Button>
+        )}
+      </Form>
+    </Admin_Page>
+  );
+};
+
+export default Serve_Form;
